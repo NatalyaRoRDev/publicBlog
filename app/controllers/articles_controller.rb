445f83a393280781ce71468154a5ffb8e246a579
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-04 16:11:32 +0300

    Form validation and created 'show.html.erb'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render action 'new'
+    end
+
   end
+
+    private
+
+    def article_params
+      params.require(:article).permit(:title, :text, :author)
+    end
+
 end
