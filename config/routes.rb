891a62f2b522bce06084a4ace9b686bef9208ae9
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-07 17:49:01 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
-
   resource :contacts, only: [:new, :create]
-  resources :articles
+  
+  resources :articles do
+    resources :comments, only: [:create]
+  end
 
 end
