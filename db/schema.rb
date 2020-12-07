91462862720e4265c76da5e9cc01e4cb37ae3f62
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-07 17:31:16 +0300

    Created a Comment table that belongs to the Article table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_113551) do
+ActiveRecord::Schema.define(version: 2020_12_07_142244) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_12_07_113551) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "contacts", force: :cascade do |t|
     t.string "email"
     t.text "message"
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_12_07_113551) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
