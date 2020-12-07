c9862bc387461ffa2fc4c6e928bdafa8a9d4ebb3
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-07 14:41:16 +0300

    Created  the Contact table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_102916) do
+ActiveRecord::Schema.define(version: 2020_12_07_113551) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
