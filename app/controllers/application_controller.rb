5baa185572312aaf784c93434cdc59264f18dca8
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-08 17:03:17 +0300

    Added username field to the form for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                               keys: [username])
+                               keys: [:username])
   end
 end
