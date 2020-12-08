1f3ec0219449e43343cd727cf04e6f4dc4023711
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-08 15:54:05 +0300

    Denied access to the articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  #get '/' => 'home#index'
   root to: "home#index"
+  
+  get "/" => "home#index"
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
